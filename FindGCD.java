600ef6f3391b80e99191aa7da8b17d59116cb208
Ahmed Abdoelrhman <82724669+Ahmed0548@users.noreply.github.com>
2023-07-30 14:24:33 +0300

    Update FindGCD.java

--- FindGCD.java
+++ FindGCD.java
@@ -2,13 +2,12 @@ class Solution {
     public int findGCD(int[] nums) {
         Arrays.sort(nums);
         int small = nums[0];
-        int large = nums[nums.length -1];
-        while(large % small != 0) {
-            int tmp = large;
-            large = small;
-            small = tmp % small;
+        int large = nums[nums.length-1];
+        for(int i = small; i >= 2; i--) {
+            if(large % i == 0 && small % i == 0) {
+                return i;
+            }
         }
-        return small;
-        
+        return 1;
     }
 }
