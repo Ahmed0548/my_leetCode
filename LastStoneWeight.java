4dac3434c83693cc2d059466dfb138a2632df711
Ahmed Abdoelrhman <82724669+Ahmed0548@users.noreply.github.com>
2023-07-31 15:31:26 +0300

    Update LastStoneWeight.java

--- LastStoneWeight.java
+++ LastStoneWeight.java
@@ -1,11 +1,11 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        int n = stones.length;
-        while(n > 1) {
+        int n = stones.length-1;
+        while(n >= 1) {
             Arrays.sort(stones);
-            int result = stones[n-1] - stones[n-2];
-            n--;
+            int result = stones[n] - stones[n-1];
             stones[n-1] = result;
+            n--;
         }
         return stones[0];
     }
