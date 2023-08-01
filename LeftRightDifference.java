73866970268dd1a759ec159428ea1c0d35de9eeb
Ahmed Abdoelrhman <82724669+Ahmed0548@users.noreply.github.com>
2023-08-01 15:29:11 +0300

    Update LeftRightDifference.java

--- LeftRightDifference.java
+++ LeftRightDifference.java
@@ -24,3 +24,27 @@ class Solution {
     return answer;        
     }
 }
+###########################################################################################################
+
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int [n];
+        int total = 0;
+
+        for(int num : nums) {
+            total += num;
+        }
+
+        int leftSum = 0;
+        int rightSum = total;
+
+        for(int i = 0; i < n; i++) {
+            rightSum -= nums[i];
+            ans[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
+        }
+        return ans;
+    }
+}
