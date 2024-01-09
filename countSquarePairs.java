bb11bab2c54af4f4f0a03ea9df49ad483e340e33
Ahmed Abdoelrhman <82724669+Ahmed0548@users.noreply.github.com>
2024-01-09 13:12:55 +0300

    Update countSquarePairs.java

--- countSquarePairs.java
+++ countSquarePairs.java
@@ -1,18 +1,25 @@
 class Solution {
-    public int countSquares(int[] arr) {
-        int count = 0;
-        for(int i = 0; i < arr.length-1; i++) {
-            for(int j = i+1; j < arr.length; j++) {
-                if(isSquare(arr[i] + arr[j])) {
-                    count++;
-                }
+   static int countSquarePairs(int[] arr) {
+    int count = 0;
+    for(int i = 0; i < arr.length; i++) {
+        for(int j = 0; j < arr.length; j++) {
+            if(arr[i] <= 0 || arr[j] <= 0) {
+                continue;
+            }
+            if(arr[i] < arr[j] && isPerfectSquare(arr[i], arr[j])) {
+                count++;
             }
         }
-        return count;
-        
-        public boolean isSquare(int n) {
-            int sqrt = (int)Math.sqrt(n);
-            return sqrt * sqrt == n;
-        }
     }
+    return count;
 }
+    static boolean isPerfectSquare(int n, int m) {
+        int num = n + m;
+        for(int i = 1; i < num; i++) {
+            if(i * i == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+}  
