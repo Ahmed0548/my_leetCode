ca5ef559c8d713fc86f11a0190d6ea6ae0ffae3c
Ahmed Abdoelrhman <82724669+Ahmed0548@users.noreply.github.com>
2024-01-18 15:52:41 +0300

    Update primeCount.java

--- primeCount.java
+++ primeCount.java
@@ -17,3 +17,26 @@ class solutions {
         return true;
     }
 }
+
+######################################################
+if start <= 1
+class solutions {
+    public static int primeCount(int start, int end) {
+    int count = 0;
+        for(int i = start; i <= end; i++) {
+            if(i <= 1) continue;
+            if(isPerfect(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean isPerfect(int num) {
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
