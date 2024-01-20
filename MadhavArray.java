ebbf1e1e407bfd636f4b315c1e6d7e1ada2275a2
Ahmed Abdoelrhman <82724669+Ahmed0548@users.noreply.github.com>
2024-01-20 10:24:15 +0300

    Update MadhavArray.java

--- MadhavArray.java
+++ MadhavArray.java
@@ -1,16 +1,18 @@
 class Solution {
     public int isMadhavArray(int[] arr) {
-        int n = (int)Math.sqrt(arr.length*2);
-        if(n*(n+1)/2 != arr.length) return 0;
-        int position = 1;
-        for(int i = 1; i <= n; i++) {
-            int sum = 0;
-            for(int j = 1; j <= i; j++) {
-                sum += arr[position++];
+        int n = (int) Math.sqrt(2 * a.length);
+        if (n * (n + 1) / 2 != a.length) {
+            return 0; 
+        }
+        int sum = a[0];
+        int i = 1;
+        for(int k = 2; k <= n; k++) {
+            int groupSum = 0;
+            for(int j = 0; j < k; j++) {
+                groupSum += a[i];
+                i++;
             }
-            if(sum != arr[0]) {
-                return 0;
-            }
+            if(groupSum != sum) return 0;
         }
         return 1;
     }
